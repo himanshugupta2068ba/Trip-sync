00fc9639756d08e1fdce03a67c759acca25c8d54
Himanshu Gupta <himanshugupta2068ba@gmail.com>
2025-09-29 23:50:44 +0530

    owenership created

--- init/index.js
+++ init/index.js
@@ -15,6 +15,10 @@ async function main() {
 
 const initDB=async () =>{
     await Listing.deleteMany({});
+   initData.data= initData.data.map((obj)=>({
+        ...obj,
+        owner:"68dac38fb32d4b67af5a8fab"
+    }));
     await Listing.insertMany(initData.data);
     console.log("data was initiallised");
 }
